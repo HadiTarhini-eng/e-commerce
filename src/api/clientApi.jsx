61de1b9cde448c54bc1822fb8d9f5f2b18610c9f
Haditarhini-eng <hadi.a.tarhini19@gmail.com>
2024-12-07 15:03:08 +0200

    update api types

--- src/api/clientApi.jsx
+++ src/api/clientApi.jsx
@@ -2,34 +2,34 @@ import { fetchData, postData } from './apiHandler';
 
 // Fetch Carousel Data
 export const fetchCarouselData = async () => {
-    return await fetchData('carousel');
+    return await fetchData('getCarouselData');
 };
 
 // Fetch Categories Data (for Home page)
 export const fetchCategoriesData = async () => {
   const page = 'Home';
-  return await fetchData('categoriesHome', { page });
+  return await fetchData('getCategoriesHomeData', { page });
 };
 
 // Fetch Categories Data (for Payment page)
 export const fetchCategoriesDataForPayment = async () => {
   const page = 'Payment';
-  return await fetchData('categoriesPayment', { page });
+  return await fetchData('getCategoriesPaymentData', { page });
 };
 
 // Fetch Products Data
 export const fetchProductsData = async () => {
-    return await fetchData('products');
+    return await fetchData('getProductsData');
 };
 
 // Fetch Ads Data
 export const fetchAdsData = async () => {
-    return await fetchData('ads');
+    return await fetchData('getAdsData');
 };
 
 // Fetch Product by ID
 export const fetchProductById = async (id) => {
-    return await fetchData('productById', { id });
+    return await fetchData('getProductById', { id });
 };
 
 // POST Request to submit the order (personal info + cart data)
